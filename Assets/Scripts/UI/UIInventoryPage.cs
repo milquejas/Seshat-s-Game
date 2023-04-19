8573c07070cda8f070f990f4f99db2a7e3bdf3fc
milquejas <84683894+milquejas@users.noreply.github.com>
2023-04-19 12:57:11 +0300

    inventoryn tekemistä
    inProgress..

---
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.RendererUtils;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryItem itemPreFab;
+    [SerializeField]
+    private RectTransform contentPanel;
+    [SerializeField]
+    private UIInventoryDescription itemDescription;
+
+
+    List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public Sprite image;
+    public int quantity;
+    public string title, description;
+
+    private void Awake()
+    {
+        Hide();
+        itemDescription.ResetDescription();
+    }
+
+    public void InitializeInventoryUI(int inventorysize)
+    {
+        
+        for (int i = 0; i < inventorysize; i++)
+        {
+            UIInventoryItem uiItem = Instantiate(itemPreFab, Vector2.zero, Quaternion.identity);
+            uiItem.transform.SetParent(contentPanel);
+            listOfUIItems.Add(uiItem);
+
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
+        } 
+    }
+
+    private void HandleShowItemActions(UIInventoryItem obj)
+    {
+      
+    }
+
+    private void HandleEndDrag(UIInventoryItem obj)
+    {
+        
+    }
+
+    private void HandleSwap(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleBeginDrag(UIInventoryItem obj)
+    {
+
+    }
+
+    private void HandleItemSelection(UIInventoryItem obj)
+    {
+        itemDescription.SetDescription(image, title, description);
+        listOfUIItems[0].Select();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+        listOfUIItems[0].SetData(image, quantity);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
