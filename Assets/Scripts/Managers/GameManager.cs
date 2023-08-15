7a4f18d18f64a76bb35e6029ad1285e06881dff5
rikutt <115475887+rikutt@users.noreply.github.com>
2023-08-15 09:06:28 +0300

    orientation fixes maybe

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -35,11 +35,6 @@ public class GameManager : MonoBehaviour
         //CheckDevice();
         versionText.text = version;
 
-        Screen.autorotateToPortrait = true;
-        Screen.autorotateToPortraitUpsideDown = true;
-        Screen.autorotateToLandscapeLeft = true;
-        Screen.autorotateToLandscapeRight = true;
-        Screen.orientation = ScreenOrientation.AutoRotation;
     }
 
     private void Awake()
--- Assets/Scripts/StartingScreen/GameStartScreenSetup.cs
+++ Assets/Scripts/StartingScreen/GameStartScreenSetup.cs
@@ -4,6 +4,15 @@ public class GameStartScreenSetup : MonoBehaviour
 {
     float startTimer;
 
+    private void Start()
+    {
+        Screen.autorotateToPortrait = true;
+        Screen.autorotateToPortraitUpsideDown = true;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+
     private void Update()
     {
         startTimer += Time.deltaTime;
